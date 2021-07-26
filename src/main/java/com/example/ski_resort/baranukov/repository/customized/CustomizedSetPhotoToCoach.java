0b4a6d7a1a84828d4402ed4bcf11f1bfdcc71a51
Rakhim Baranukov <idj4me@mail.ru>
2021-07-26 14:04:55 +0300

    Removed files

--- src/main/java/com/example/ski_resort/baranukov/repository/customized/CustomizedSetPhotoToCoach.java
+++
@@ -1,9 +0,0 @@
-package com.example.ski_resort.baranukov.repository.customized;
-
-import com.example.ski_resort.baranukov.entity.Coach;
-
-public interface CustomizedSetPhotoToCoach {
-
-    void setPhotoToCoach(Coach coach, String pathNameToPhoto);
-
-}
